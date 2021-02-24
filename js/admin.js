ee0c5fcc9697d1966d14bfa62340adb6d9e63a03
Martin <martincordich@gmail.com>
2021-02-24 11:29:23 -0300

    se agregaron variables en admin js

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,24 @@
 import { Funko } from "./funkoClass.js"
 
-let nuevoFunkopop = new Funko(1, 'Ironman', 'M51325', 'Marvel', 'Extremis', 'no disponible');
+let listaFunkopop = []
 
-console.log(nuevoFunkopop);
+
+
+window.agregarFunkopop = function(event) {
+    //el objetivo de esta funcion es agregar un funkopop nuevo en el localstorage
+    event.preventDefault();
+    console.log('estamos dentro de la funcion funkopop')
+        //traer los valores del formulario que ya estan validados
+
+    let codigo = document.getElementById('codigo').value;
+    let nombre = document.getElementById('nombre').value;
+    let numSerie = document.getElementById('numSerie').value;
+    let categoria = document.getElementById('categoria').value;
+    let descripcion = document.getElementById('descripcion').value;
+    let imagen = document.getElementById('imagen').value;
+
+    let nuevoFunkopop = new Funko(codigo, nombre, numSerie, categoria, descripcion, imagen);
+    //agregar el nmuevo objeto en el arreglo de funkopop
+    listaFunkopop.push(nuevoFunkopop);
+    console.log(listaFunkopop);
+}
